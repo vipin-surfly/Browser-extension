4c3fd0e2f08dbbd3ced115832d4d61fbb3eca53c
vipin-surfly <100697737+vipin-surfly@users.noreply.github.com>
2022-03-27 00:32:15 +0100

    Delete findelementclass.js

--- findelementclass.js
+++
@@ -1,8 +0,0 @@
-var oldel;
-
-$(document).on("click", function(event){
-    $(oldel).removeProp("border");
-    $(event.target).css('border', '1px solid black');
-    console.log(event.target.className);
-    event.target=oldel;
-})
